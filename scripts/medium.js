c2ef43e06360d072d7a390ad3680292b84373812
Bhanot, Karan <k.bhanot@cvent.com>
2019-01-29 00:26:28 +0530

    Use cors-anywhere to fetch Medium data

--- scripts/medium.js
+++ scripts/medium.js
@@ -1,4 +1,4 @@
-const mediumRequest = fetch('https://medium.com/@bhanotkaran22/latest')
+const mediumRequest = fetch('https://cors-anywhere.herokuapp.com/https://medium.com/@bhanotkaran22/latest')
 .then(function(response) {
 	if (!response.ok) {
        	return Promise.reject('Could not fetch user information from Medium');
