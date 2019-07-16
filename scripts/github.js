a87fcf4e74285382461a37a8c5bf6d74b65bc876
Karan Bhanot <bhanotkaran22@gmail.com>
2019-07-16 11:13:55 +0530

    Add two column view for more readability and better appearance

--- scripts/github.js
+++ scripts/github.js
@@ -5,53 +5,75 @@ fetch('https://api.github.com/users/kb22/repos?sort=pushed')
     }
     return response.json();
 }).then(text => {
-	plotMultiColRepos(text);
+	singleColRepo(text);
 }).catch(error => {
 	console.log(error);
 });
 
-function plotMultiColRepos(text) {
+function singleColRepo(text) {
 	let content = document.getElementsByClassName("feature-content")[0];
 	let count = 0;
-	const rowsCount = (text.length)%3;
-	for (let j = 0; j < 10; j++) {
+	for (let j = 0; j < 15; j++) {
 		let row = document.createElement('div');
 		row.setAttribute('class', 'row');
-		row.style.marginTop = '20px';
+		for (let i = count; i < count + 2; i++) {
+			row.style.marginTop = '20px';
+			row.style.marginBottom = '20px';
 
-		for (let i = count; i < count + 3; i++) {
-			let col = document.createElement('div');
-			col.setAttribute('class', 'col-md-4');
+			let col1 = document.createElement('div');
+			col1.setAttribute('class', 'col-md-2');
 
 			let image = document.createElement('img');
 			image.setAttribute('src', 
-				'https://images.unsplash.com/photo-1560620564-850829217d97?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=280&ixlib=rb-1.2.1&q=80&w=280');
-			
-			let heading = document.createElement('h5');
-			heading.textContent = text[i].name;
+				'https://images.unsplash.com/photo-1562369865-0a516793ce29?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=240&ixlib=rb-1.2.1&q=80&w=240');
+
+			let col2 = document.createElement('div');
+			col2.setAttribute('class', 'col-md-4');
+
+			let heading = document.createElement('h6');
+			heading.textContent = (text[i].name.split('-')).join(' ');
 
 			let description = document.createElement('p');
 			description.textContent = text[i].description;
 
-			let link = document.createElement('a');
-			link.setAttribute('href', text[i].html_url);
-			link.setAttribute('role', 'button');
-			link.setAttribute('target', '_blank');
-			link.setAttribute('class', 'btn btn-outline-dark theme-btn');
-			link.textContent = "Project "
+			let info = document.createElement('p');
+			let starIcon = document.createElement('i');
+			starIcon.setAttribute('class', 'fa fa-star info-icon');
+			info.appendChild(starIcon);
+			let starCount = document.createElement('span');
+			starCount.textContent = text[i].stargazers_count;
+			info.appendChild(starCount);
+			let forkIcon = document.createElement('i');
+			forkIcon.setAttribute('class', 'fa fa-code-fork info-icon');
+			info.appendChild(forkIcon);
+			let forkCount = document.createElement('span');
+			forkCount.textContent = text[i].forks;
+			info.appendChild(forkCount);
 
-			let icon = document.createElement('i');
-			icon.setAttribute('class', 'fa fa-github social');
+			let links = document.createElement('p');
+			let codeLink = document.createElement('a');
+			codeLink.setAttribute('href', text[i].html_url);
+			codeLink.setAttribute('target', '_blank');
+			codeLink.textContent = "View Source Code";
+			links.append(codeLink);
+			let dot = document.createElement('i');
+			dot.setAttribute('class', 'fa fa-circle-thin separator');
+			links.appendChild(dot);
+			let readme = document.createElement('a');
+			readme.setAttribute('href', text[i].html_url + '/blob/master/README.md');
+			readme.setAttribute('target', '_blank');
+			readme.textContent = "Readme";
+			links.append(readme);
 
-			link.appendChild(icon);
-			col.appendChild(image);
-			col.appendChild(heading);
-			col.appendChild(description);
-			col.appendChild(link);
-			row.appendChild(col);
+			col1.appendChild(image);
+			col2.appendChild(heading);
+			col2.appendChild(description);
+			col2.appendChild(info);
+			col2.appendChild(links);
+			row.appendChild(col1);
+			row.appendChild(col2);
 		}
-
 		content.appendChild(row);
-		count +=  4;
+		count += 2;
 	}
 }
