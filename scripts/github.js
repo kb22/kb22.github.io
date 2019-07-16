40658c5fa6a92c2d32de3d921386b14d3d52cfaf
Karan Bhanot <bhanotkaran22@gmail.com>
2019-07-16 16:22:07 +0530

    Add support for showing more than 30 projects from GitHub

--- scripts/github.js
+++ scripts/github.js
@@ -1,22 +1,22 @@
-fetch('https://api.github.com/users/kb22/repos?sort=pushed')
+fetch('https://api.github.com/users/kb22/repos?sort=pushed&per_page=30')
 .then(response => {
 	if (!response.ok) {
        return Promise.reject('Could not fetch repos information from GitHub');
     }
     return response.json();
 }).then(text => {
-	singleColRepo(text);
+	showProjects(text);
 }).catch(error => {
 	console.log(error);
 });
 
-function singleColRepo(text) {
+function showProjects(text) {
 	let content = document.getElementsByClassName("feature-content")[0];
 	let count = 0;
-	for (let j = 0; j < 15; j++) {
+	for (let j = 0; j < (text.length/2); j++) {
 		let row = document.createElement('div');
 		row.setAttribute('class', 'row');
-		for (let i = count; i < count + 2; i++) {
+		for (let i = count; (i < count + 2) && (i < text.length); i++) {
 			row.style.marginTop = '20px';
 			row.style.marginBottom = '20px';
 
@@ -64,7 +64,7 @@ function singleColRepo(text) {
 			readme.textContent = "Readme";
 			links.append(readme);
 
-			if (text[i].homepage != null) {
+			if (text[i].homepage != null && text[i].homepage != "") {
 				dot = document.createElement('i');
 				dot.setAttribute('class', 'fa fa-circle-thin separator');
 				links.appendChild(dot);
@@ -86,4 +86,30 @@ function singleColRepo(text) {
 		content.appendChild(row);
 		count += 2;
 	}
+
+	if (text.length == 30) {
+		page_no += 1;
+		let moreButton = document.createElement('a');
+		moreButton.setAttribute('class', 'btn btn-outline-dark theme-btn');
+		moreButton.setAttribute('role', 'button');
+		moreButton.setAttribute('target', '_blank');
+		moreButton.onclick = loadMoreProjects;
+		moreButton.textContent = 'Load more';
+		content.appendChild(moreButton);
+	}
+}
+
+function loadMoreProjects() {
+	this.style.display = 'none';
+	fetch('https://api.github.com/users/kb22/repos?sort=pushed&per_page=30&page=' + page_no)
+	.then(response => {
+		if (!response.ok) {
+	       return Promise.reject('Could not fetch repos information from GitHub');
+	    }
+	    return response.json();
+	}).then(text => {
+		showProjects(text);
+	}).catch(error => {
+		console.log(error);
+	});
 }
