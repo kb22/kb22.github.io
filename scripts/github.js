1df05ea1714b61743a08bdbaedf49fa12103b866
Karan Bhanot <bhanotkaran22@gmail.com>
2020-05-21 04:20:19 -0400

    Update styles and add footer

--- scripts/github.js
+++ scripts/github.js
@@ -38,54 +38,55 @@ function showProjects(text) {
 		let description = document.createElement('p');
 		row.style.fontSize = '12px';
 		description.textContent = text[i].description;
+		description.style.textAlign = "justify";
 
 		let info = document.createElement('p');
-			let starIcon = document.createElement('i');
-			starIcon.setAttribute('class', 'fa fa-star info-icon');
-			info.appendChild(starIcon);
-			let starCount = document.createElement('span');
-			starCount.textContent = text[i].stargazers_count;
-			info.appendChild(starCount);
-			let forkIcon = document.createElement('i');
-			forkIcon.setAttribute('class', 'fa fa-code-fork info-icon');
-			info.appendChild(forkIcon);
-			let forkCount = document.createElement('span');
-			forkCount.textContent = text[i].forks;
-			info.appendChild(forkCount);
+		let starIcon = document.createElement('i');
+		starIcon.setAttribute('class', 'fa fa-star info-icon');
+		info.appendChild(starIcon);
+		let starCount = document.createElement('span');
+		starCount.textContent = text[i].stargazers_count;
+		info.appendChild(starCount);
+		let forkIcon = document.createElement('i');
+		forkIcon.setAttribute('class', 'fa fa-code-fork info-icon');
+		info.appendChild(forkIcon);
+		let forkCount = document.createElement('span');
+		forkCount.textContent = text[i].forks;
+		info.appendChild(forkCount);
 
-			let links = document.createElement('p');
-			let codeLink = document.createElement('a');
-			codeLink.setAttribute('href', text[i].html_url);
-			codeLink.setAttribute('target', '_blank');
-			codeLink.textContent = "Source Code";
-			links.append(codeLink);
-			let dot = document.createElement('i');
+		let links = document.createElement('p');
+		let codeLink = document.createElement('a');
+		codeLink.setAttribute('href', text[i].html_url);
+		codeLink.setAttribute('target', '_blank');
+		codeLink.textContent = "Source Code";
+		links.append(codeLink);
+		let dot = document.createElement('i');
+		dot.setAttribute('class', 'fa fa-circle-thin separator');
+		links.appendChild(dot);
+		let readme = document.createElement('a');
+		readme.setAttribute('href', text[i].html_url + '/blob/master/README.md');
+		readme.setAttribute('target', '_blank');
+		readme.textContent = "Readme";
+		links.append(readme);
+
+		if (text[i].homepage != null && text[i].homepage != "") {
+			dot = document.createElement('i');
 			dot.setAttribute('class', 'fa fa-circle-thin separator');
 			links.appendChild(dot);
-			let readme = document.createElement('a');
-			readme.setAttribute('href', text[i].html_url + '/blob/master/README.md');
-			readme.setAttribute('target', '_blank');
-			readme.textContent = "Readme";
-			links.append(readme);
+			let article = document.createElement('a');
+			article.setAttribute('href', text[i].homepage);
+			article.setAttribute('target', '_blank');
+			article.textContent = "Article";
+			links.append(article);
+		}
 
-			if (text[i].homepage != null && text[i].homepage != "") {
-				dot = document.createElement('i');
-				dot.setAttribute('class', 'fa fa-circle-thin separator');
-				links.appendChild(dot);
-				let article = document.createElement('a');
-				article.setAttribute('href', text[i].homepage);
-				article.setAttribute('target', '_blank');
-				article.textContent = "Article";
-				links.append(article);
-			}
-
-			col1.appendChild(image);
-			col2.appendChild(heading);
-			col2.appendChild(description);
-			col2.appendChild(info);
-			col2.appendChild(links);
-			row.appendChild(col1);
-			row.appendChild(col2);
+		col1.appendChild(image);
+		col2.appendChild(heading);
+		col2.appendChild(description);
+		col2.appendChild(info);
+		col2.appendChild(links);
+		row.appendChild(col1);
+		row.appendChild(col2);
 		content.appendChild(row);
 	}
 	// for (let j = 0; j < (text.length/2); j++) {
