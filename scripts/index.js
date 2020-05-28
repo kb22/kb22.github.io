124a6f141a64ed173930b7e6d47301ad1c94e52c
Karan Bhanot <bhanotkaran22@gmail.com>
2020-05-28 14:33:13 -0400

    Make navbar responsive to screen size and toggle button

---
+++ scripts/index.js
@@ -0,0 +1,26 @@
+$(document).ready(function(e){
+	$win = $(window);
+	$navbar = $("#header");
+	$toggle = $(".toggle-button");
+	var width = $navbar.width();
+
+	toggleOnClick($win, $navbar, width);
+
+	//resize event
+	$win.resize(function(){
+		toggleOnClick($win, $navbar, width);
+	});
+
+	$toggle.click(function(){
+		$navbar.toggleClass("toggle-left");
+	});
+
+});
+
+function toggleOnClick($win, $navbar, width) {
+	if ($win.width() <= 768) {
+		$navbar.css({left: `-${width}px`});
+	} else {
+		$navbar.css({left: '0px'});
+	}
+}
